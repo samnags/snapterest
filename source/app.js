64130f72de71e190987784bdfdbf6a817bec5f15
samnags <sam.nagourney@gmail.com>
2016-08-09 11:16:13 -0400

    chapter 4 done

--- source/app.js
+++ source/app.js
@@ -1,2 +1,49 @@
 var React = require('react');
 var ReactDOM = require('react-dom')
+var Application = require('./components/Application.react')
+
+
+
+ReactDOM.render(
+  <Application  />,
+  document.getElementById('react-application')
+)
+
+
+// var MessageComponent = React.createClass({
+//   onClickityClick() {
+//     alert("Bonjour friend")
+//   },
+//
+//   render() {
+//     return (
+//       <div>
+//         <button className="btn btn" onClick={this.onClickityClick}>Click Me. You know you want to</button>
+//       </div>
+//     )
+//   }
+// })
+
+
+// var MyForm = React.createClass({
+//   getInitialState: function() {
+//     return {value: 'Hello!'};
+//   },
+//
+//   handleChange: function(event) {
+//     this.setState({value: event.target.value});
+//   },
+//
+//   render: function() {
+//     return (
+//       <input
+//         type="text"
+//         value={this.state.value}
+//         onChange={this.handleChange}
+//       />
+//     );
+//   }
+//
+// })
+//
+// var MyComponent = React.createElement(MyForm);
--- source/components/Application.react.js
+++ source/components/Application.react.js
@@ -10,4 +10,54 @@ var Application = React.createClass({
     };
   },
 
-})
+  addTweetToCollection: function(tweet) {
+    var collectionTweets = this.state.collectionTweets;
+
+    collectionTweets[tweet.id] = tweet;
+
+
+    // setState is a React function. Takes in argument of what you want to change about state
+    this.setState({
+      collectionTweets: collectionTweets
+    });
+  },
+
+  removeAllTweetsFromCollection: function() {
+    this.setState({
+      collectionTweets: {}
+    });
+  },
+
+  removeTweetFromCollection: function(tweet) {
+    var collectionTweets = this.state.collectionTweets;
+
+    delete collectionTweets[tweet.id];
+
+    this.setState({
+      collectionTweets: collectionTweets
+    });
+  },
+
+  render: function() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-4 text-center">
+            // parent passing a callback function as property to child components
+            // this is how Stream can update application's state
+            <Stream onAddTweetToCollection = {this.addTweetToCollection} />
+          </div>
+
+        <div className="col-md-8">
+          <Collection
+            tweets={this.state.collectionTweets}
+            onRemoveTweeetFromCollection={this.removeTweetFromCollection}
+            onRemoveAllTweetsFromCollection={this.removeAllTweetsFromCollection} />
+            </div>
+          </div>
+        </div>
+    );
+  }
+});
+
+module.exports = Application;
